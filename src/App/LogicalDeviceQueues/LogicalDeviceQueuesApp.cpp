563818bfbb6165ff708c199ea041097436f24314
rge15 <78486074+rge15@users.noreply.github.com>
2023-03-10 09:55:29 +0100

    Update LogicalDeviceQueuesApp.cpp

--- src/App/LogicalDeviceQueues/LogicalDeviceQueuesApp.cpp
+++ src/App/LogicalDeviceQueues/LogicalDeviceQueuesApp.cpp
@@ -7,9 +7,4 @@ LogicaDeviceQueuesApp::run()
 {
     UniqPtr<InstanceMng> instanceMng = std::make_unique<InstanceMng>();
     UniqPtr<DeviceMng_LogicalAPP> deviceMng_LogicalAPP = std::make_unique<DeviceMng_LogicalAPP>( instanceMng.get()->_vkInstance );
-
-
-    //auto instMg = std::make_unique<InstanceMng>();
-    //auto inst   = instMg.get()->_vkInstance;
-    //auto devApp = std::make_unique<DeviceMng_LogicalAPP>( inst );
 }
